9c1e0a064903411acc5211b03a298403a51b9a3f
abbas.khalili <khalilikhoo2@gmail.com>
2022-11-17 22:21:24 +0330

    make a lib for logging by NLog

---
+++ framework/Framework.Logger/NLogConfigure.cs
@@ -0,0 +1,12 @@
+﻿using NLog;
+
+namespace Framework.Logger
+{
+    public static class NLogConfigure
+    {
+        public static void Config(string nlogConfigFile = "./nlog.config")
+        {
+            LogManager.LoadConfiguration(nlogConfigFile).GetCurrentClassLogger();
+        }
+    }
+}
---
+++ framework/Framework.Logger/NLogService.cs
@@ -0,0 +1,62 @@
+﻿using Framework.Core;
+using NLog;
+using System;
+
+namespace Framework.Logger
+{
+    public abstract class NLogService
+    {
+        private readonly ILogger _logger;
+
+        protected NLogService(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Information(string message)
+        {
+            _logger.Info(message);
+        }
+
+        public void Warning(string message)
+        {
+            _logger.Warn(message);
+        }
+
+        public void Debug(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void Error(string message)
+        {
+            _logger.Error(message);
+        }
+
+        public void Error(Exception exception)
+        {
+            _logger.Error(exception);
+        }
+
+        public void Error(Exception exception, string message)
+        {
+            _logger.Error(exception, message);
+        }
+    }
+    public class NLoggerService : NLogService, ILoggerService
+    {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+        public NLoggerService() : base(Logger)
+        {
+        }
+    }
+
+    public class NLoggerService<T> : NLogService, ILoggerService<T> where T : class
+    {
+        private static readonly ILogger Logger = LogManager.LogFactory.GetLogger(typeof(T).FullName);
+
+        public NLoggerService() : base(Logger)
+        {
+
+        }
+    }
+}
