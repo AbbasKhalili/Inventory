c2c9138e09f516011765703f8108ad814aff187d
abbas.khalili <khalilikhoo2@gmail.com>
2022-11-17 22:31:37 +0330

    create writemodel side

---
+++ src/Inventory.Interface.WriteModel/CategoryFacadeService.cs
@@ -0,0 +1,26 @@
+﻿using Framework.Application;
+using Inventory.Application.Contract.Categories;
+using Inventory.Interface.Contract.Category.Models;
+using Inventory.Interface.Contract.Category.Services;
+using System.Threading.Tasks;
+
+namespace Inventory.Interface.WriteModel
+{
+    public class CategoryFacadeService : ICategoryFacadeService
+    {
+        private readonly ICommandBus _commandBus;
+
+        public CategoryFacadeService(ICommandBus commandBus)
+        {
+            _commandBus = commandBus;
+        }
+
+        public async Task Create(CategoryModel model)
+        {
+            await _commandBus.Dispatch(new CreateCategoryCommand
+            {
+                Name = model.Name
+            });
+        }
+    }
+}
