cec98275b9303ed98c8ac9c966a90c3f428056ae
abbas.khalili <khalilikhoo2@gmail.com>
2022-11-17 22:29:42 +0330

    added Bootstrap for Inventory to wireup it

---
+++ src/Inventory.Bootstrap/InventoryModule.cs
@@ -0,0 +1,66 @@
+﻿using Autofac;
+using System;
+using System.Linq;
+using Inventory.Application.CategoryHandlers;
+using Inventory.Persistence.Mappings;
+using Framework.Application;
+using Framework.Core;
+using Framework.Domain;
+using Microsoft.EntityFrameworkCore;
+using Inventory.Persistence.Repositories;
+using Inventory.Interface.WriteModel;
+using Inventory.Interface.QueryModel;
+using Framework.DataAccess.EF;
+
+namespace Inventory.Bootstrap
+{
+    internal class InventoryModule : Module
+    {
+        private readonly string _connectionString;
+
+        public InventoryModule(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(CategoryRepository).Assembly)
+                .Where(a => typeof(IRepository).IsAssignableFrom(a))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(typeof(CategoryFacadeQuery).Assembly)
+                .Where(a => typeof(IFacadeService).IsAssignableFrom(a))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(typeof(CategoryFacadeService).Assembly)
+                .Where(a => typeof(IFacadeService).IsAssignableFrom(a))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+
+            //builder.RegisterAssemblyTypes(typeof(ValidateNationalIdentity).Assembly)
+            //    .Where(a => typeof(IDomainService).IsAssignableFrom(a))
+            //    .AsImplementedInterfaces()
+            //    .InstancePerLifetimeScope();
+
+
+            builder.RegisterAssemblyTypes(typeof(CategoryCommandHandler).Assembly)
+                .AsClosedTypesOf(typeof(ICommandHandler<>)).InstancePerLifetimeScope();
+
+
+            builder.Register<EFContext>(s =>
+            {
+                var options = new DbContextOptionsBuilder<EFContext>()
+                    .UseSqlServer(_connectionString)
+                    .EnableSensitiveDataLogging(true)
+                    .LogTo(Console.Write, Microsoft.Extensions.Logging.LogLevel.Information)
+                    .Options;
+                var context = new EFContext(options, typeof(CategoryMap).Assembly);
+                return context;
+            }).As<EFContext>().As<IDbContext>().InstancePerLifetimeScope().OnRelease(a => a.Dispose());
+        }
+    }
+}
---
+++ src/Inventory.Bootstrap/RegisterInventoryModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace Inventory.Bootstrap
+{
+    public static class RegisterInventoryModule
+    {
+        public static void AddModule(this ContainerBuilder builder, string connectionString)
+        {
+            builder.RegisterModule(new InventoryModule(connectionString));
+        }
+    }
+}
