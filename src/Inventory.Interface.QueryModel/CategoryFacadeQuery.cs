fd4b6d78afed2e708f82423fb0d9da4085850abd
abbas.khalili <khalilikhoo2@gmail.com>
2022-11-17 22:31:14 +0330

    added ReadModel

---
+++ src/Inventory.Interface.QueryModel/CategoryFacadeQuery.cs
@@ -0,0 +1,25 @@
+﻿using Inventory.Domain.Categories;
+using Inventory.Interface.Contract.Category.DTOs;
+using Inventory.Interface.Contract.Category.Services;
+using Inventory.Interface.QueryModel.Mappers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Inventory.Interface.QueryModel
+{
+    public class CategoryFacadeQuery : ICategoryFacadeQuery
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryFacadeQuery(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<List<CategoryDto>> GetAll()
+        {
+            var list = await _categoryRepository.GetAll();
+            return CategoryMapper.Map(list);
+        }
+    }
+}
---
+++ src/Inventory.Interface.QueryModel/Mappers/CategoryMapper.cs
@@ -0,0 +1,24 @@
+﻿using Inventory.Domain.Categories;
+using Inventory.Interface.Contract.Category.DTOs;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Inventory.Interface.QueryModel.Mappers
+{
+    internal static class CategoryMapper
+    {
+        public static List<CategoryDto> Map(List<Category> list)
+        {
+            return list.Select(a => Map(a)).ToList();
+        }
+
+        public static CategoryDto Map(Category category)
+        {
+            return new CategoryDto
+            {
+                Id = category.Id.SurrogateKey,
+                Name = category.Name
+            };
+        }
+    }
+}
