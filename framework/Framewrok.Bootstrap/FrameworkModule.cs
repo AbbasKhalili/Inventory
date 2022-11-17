bbef2034fe331bee0c5a2a51e6f278dacd3d663b
abbas.khalili <khalilikhoo2@gmail.com>
2022-11-17 22:23:03 +0330

    create Bootstrap lib in framework to wireup system

---
+++ framework/Framewrok.Bootstrap/FrameworkModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Framework.Application;
+using Framework.Core;
+using Framework.DataAccess.EF;
+
+namespace Framewrok.Bootstrap
+{
+    public class FrameworkModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterGenericDecorator(typeof(TransactionalCommandHandlerDecorator<>), typeof(ICommandHandler<>));
+
+            builder.RegisterType<CommandBus>().As<ICommandBus>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<ServiceLocator>().As<IServiceLocator>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
---
+++ framework/Framewrok.Bootstrap/ServiceLocator.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Framework.Core;
+
+namespace Framewrok.Bootstrap
+{
+    public class ServiceLocator : IServiceLocator
+    {
+        private readonly ILifetimeScope _lifetimeScope;
+        public ServiceLocator(ILifetimeScope lifetimeScope)
+        {
+            _lifetimeScope = lifetimeScope;
+        }
+
+        public T GetInstance<T>() where T : class
+        {
+            return _lifetimeScope.Resolve<T>();
+        }
+    }
+}
